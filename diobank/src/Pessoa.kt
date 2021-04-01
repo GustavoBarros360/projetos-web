37984437afff11442c17316b31b7559513b021d8
Gustavo <gustavo_barros@usp.br>
2021-04-01 10:49:59 -0300

    updated to aply the orivate to set function of cpf properti

--- diobank/src/Pessoa.kt
+++ diobank/src/Pessoa.kt
@@ -1,4 +1,18 @@
+package one.digitalinnovation.digionebank
+
 class Pessoa {
-    val name:String
-    val cpf:String
+    var name:String = "Gustavo"
+
+    var cpf:String = "46220922808"
+    private set
+
+}
+
+fun main(){
+    val gustavo = Pessoa()
+    //gustavo.cpf = "456"
+    println(gustavo.name)
+    println(gustavo.cpf)
+
+
 }

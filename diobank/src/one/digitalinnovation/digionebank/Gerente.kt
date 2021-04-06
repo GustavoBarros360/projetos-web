3b6d187a1b1786dcb3759badf11103037df927f9
Gustavo <gustavo_barros@usp.br>
2021-04-06 11:00:17 -0300

    first commit
    calculadora de notas project created

--- diobank/src/one/digitalinnovation/digionebank/Gerente.kt
+++ diobank/src/one/digitalinnovation/digionebank/Gerente.kt
@@ -3,10 +3,14 @@ package one.digitalinnovation.digionebank
 class Gerente(
     name: String,
     cpf: String,
-    salary: Double
-) : Funcionario(name, cpf, salary) {
+    salary: Double,
+    val senha: String
+) : Funcionario(name, cpf, salary), logavel {
     override fun calculoAuxilio(): Double {
         return salary*0.4
     }
+
+    override fun login():Boolean =
+        "123456" == senha
 
 }
--- diobank/src/one/digitalinnovation/digionebank/testes/TesteGerente.kt
+++ diobank/src/one/digitalinnovation/digionebank/testes/TesteGerente.kt
@@ -6,8 +6,8 @@ import one.digitalinnovation.digionebank.Gerente
 
 fun main(){
     val joao = Gerente(name = "João", cpf = "123456"
-        , salary = 5000.0)
+        , salary = 5000.0, senha = "123456")
 
-    
+
 }
 
